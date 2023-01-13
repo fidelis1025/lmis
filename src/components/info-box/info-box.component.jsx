190a5384084f34ae19a07216b1e13983deabccb7
Undie Fidelis <undiefidelis@gmail.com>
2023-01-13 14:10:00 +0100

    added state to the infobox component

--- src/components/info-box/info-box.component.jsx
+++ src/components/info-box/info-box.component.jsx
@@ -16,24 +16,33 @@ class InfoBox extends React.Component {
           id: 2,
         },
         {
-            value: "9.79 %",
-            description: "Employment rate Macrotrends, 2022",
-            id: 3,
-          },
-          {
-            value: "35,987",
-            description: "Number of registered businesses CAC, 2022",
-            id: 4,
-          },
-          {
-            value: "43.6%",
-            description: "% of working population 18 & older NBS, 2022",
-            id: 5,
-          },
+          value: "9.79 %",
+          description: "Employment rate Macrotrends, 2022",
+          id: 3,
+        },
+        {
+          value: "35,987",
+          description: "Number of registered businesses CAC, 2022",
+          id: 4,
+        },
+        {
+          value: "43.6%",
+          description: "% of working population 18 & older NBS, 2022",
+          id: 5,
+        },
       ],
     };
   }
-}
 
+  render(){
+    return(
+        <div className="absolute left-[9rem] top-[32rem]">
+            <div >
+
+            </div>
+        </div>
+    )
+  }
+}
 
 export default InfoBox;
