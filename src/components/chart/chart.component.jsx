d34a67b3e58fa7c4063d14f436956172fa513aea
Undie Fidelis <undiefidelis@gmail.com>
2023-01-23 11:52:45 +0100

    added new tailwind config file

--- src/components/chart/chart.component.jsx
+++ src/components/chart/chart.component.jsx
@@ -1,20 +1,20 @@
 import React from "react"
 
 import image2 from "../../images/nigeria-unemployment-rate-2022-09-27.png";
-import image3 from "../../images/group.png";
+import image3 from "../../images/Group.png";
 
 
-const ChartItems = ()=> (
-    <div className="w-full h-[10rem] flex space-x-4 mx-8">
+const ChartItems = () => (
+    <div className="w-full h-[10rem] flex space-x-4 mt-[15rem]">
         <div>
 
-        <img src={image2} alt="" />
-        <p></p>
+        <img className="w-[37rem] h-[22rem]" src={image2} alt="" />
+        <p className="my-4 text-center text-sm text-[#1E5811]">Unemployment rate in Nigeria 1991-2022. Data source: World Bank, 2022</p>
         </div>
         <div>
 
-        <img src={image3} alt="" />
-        <p></p>
+        <img className="w-[24.3rem] h-[22rem]" src={image3} alt="" />
+        <p className=" my-4 text-center text-sm text-[#1E5811]">Nigeria employment statistics by sector, 2021</p>
         </div>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js, jsx, ts, tsx}",
-    "./src/**/*"
-  ],
+  content: [],
   theme: {
-    extend: {
-      color: {
-        'colorGreen': 'hsla(104, 43%, 35%, 0.85)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
